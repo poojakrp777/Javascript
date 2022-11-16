f979773efee61e263b888637d50206a1d02f6cde
poojakrp777 <poojakrp777@gmail.com>
2022-11-16 15:19:39 +0530

    javascript basic program on switch case

---
+++ num.js
@@ -0,0 +1,28 @@
+var readlineSync=require('readline-sync');
+var num=readlineSync.question('Enter the number');
+switch(num)
+{
+    case "1":
+        console.log('unit');
+    break;
+
+    case "1":
+        console.log('unit');
+    break;
+
+    case "2":
+        console.log('TEN');
+    break;
+
+    case "3":
+        console.log('HUNDRED');
+    break;
+
+    case "4":
+        console.log('THOUSAND');
+    break;
+
+    case "5":
+        console.log('TEN THOUSAND');
+    break;
+}
